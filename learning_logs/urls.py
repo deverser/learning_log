a29cb67c782c7c28e61ed75964897ec220762065
deverser <alexmal74@gmail.com>
2019-07-01 00:47:22 +0500

    added routing for "topics" page, created function for data processing of topics request from user

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,4 +8,6 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Домашняя страница
     path('', views.index, name='index'),
+    # Вывод всех тем
+    path('topics/', views.topics, name='topics'),
 ]
