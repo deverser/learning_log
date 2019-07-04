915621b86297f5966f39f45e3b4ef7a4b51ca407
Alexander <alexmal74@gmail.com>
2019-07-04 11:34:30 +0500

    Added url routing and data request function for single topic

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     # Вывод всех тем
     path('topics/', views.topics, name='topics'),
+    # Подробная страница одной темы
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
