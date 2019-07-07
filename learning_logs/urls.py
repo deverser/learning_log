e72c2ddebbd3338e6dd80457d676f0c93f2a3f9e
deverser <alexmal74@gmail.com>
2019-07-08 00:38:01 +0500

    Changed order of URLs in url-patterns for performance optimization

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -6,16 +6,16 @@ from . import views
 
 app_name = 'learning_logs'
 urlpatterns = [
-    # Домашняя страница
-    path('', views.index, name='index'),
-    # Вывод всех тем
-    path('topics/', views.topics, name='topics'),
-    # Подробная страница одной темы
-    path('topics/<int:topic_id>/', views.topic, name='topic'),
-    # Страница для добавления новой темы
-    path('new_topic/', views.new_topic, name='new_topic'),
     # Страница для добавления нового сообщения
     path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
     # Страница редактирования записи
     path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
+    # Подробная страница одной темы
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    # Страница для добавления новой темы
+    path('new_topic/', views.new_topic, name='new_topic'),
+    # Вывод всех тем
+    path('topics/', views.topics, name='topics'),
+    # Домашняя страница
+    path('', views.index, name='index'),
 ]
