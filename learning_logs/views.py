a13a110a2d2b159da990b1d9a46cb202fee3a7e4
deverser <alexmal74@gmail.com>
2019-07-21 23:24:50 +0500

    Added access control for logged users

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -22,6 +22,7 @@ def topics(request):
     return render(request, 'learning_logs/topics.html', context)
 
 
+@login_required
 def topic(request, topic_id):
     """Выводит одну тему и все её записи"""
     topic = Topic.objects.get(id=topic_id)
@@ -30,6 +31,7 @@ def topic(request, topic_id):
     return render(request, 'learning_logs/topic.html', context)
 
 
+@login_required
 def new_topic(request):
     """Определяет новую тему"""
     if request.method != 'POST':
@@ -45,6 +47,7 @@ def new_topic(request):
     return render(request, 'learning_logs/new_topic.html', context)
 
 
+@login_required
 def new_entry(request, topic_id):
     """Добавляет новую запись по конкретной теме"""
     topic = Topic.objects.get(id=topic_id)
@@ -64,6 +67,7 @@ def new_entry(request, topic_id):
     return render(request, 'learning_logs/new_entry.html', context)
 
 
+@login_required
 def edit_entry(request, entry_id):
     """Редактирует существующую запись"""
     entry = Entry.objects.get(id=entry_id)
